095b68a962b59b701f6f4f79f5f206ac7e7dff65
GM TARIQ COMPUTERS <mzainkhalid397@gmail.com>
2024-12-10 13:45:22 +0500

    String Building and Counting

--- 1. go_syntax/cmd/lesson_1/main.go
+++ 1. go_syntax/cmd/lesson_1/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"strings"
 	"time"
 	"unicode/utf8"
 )
@@ -16,7 +17,10 @@ func main() {
 	// arr_slices_loops()
 
 	//3.
-	arr_mem_allocation_speedtest()
+	// arr_mem_allocation_speedtest()
+
+	//4.
+	strings_runes()
 }
 
 // 1.
@@ -168,6 +172,7 @@ func arr_slices_loops() {
 
 }
 
+// 3.
 func arr_mem_allocation_speedtest() {
 	var n = 1000000
 
@@ -180,6 +185,7 @@ func arr_mem_allocation_speedtest() {
 
 }
 
+// 3.1
 func timeloop(mySlice []int, n int) time.Duration {
 
 	t0 := time.Now()
@@ -189,5 +195,49 @@ func timeloop(mySlice []int, n int) time.Duration {
 	}
 
 	return time.Since(t0)
+
+}
+
+// 4.
+func strings_runes() {
+	var myString = "rèsumè"
+	fmt.Printf("Length of a String using len is invalid due to underlying dynamic byte encoding of utf8 = %v\n", len(myString))
+	fmt.Printf("Indexing a string gives a number = %v\n", myString[0])
+
+	for i, v := range myString {
+		fmt.Printf("%v => %v\n", i, v)
+	}
+
+	// Index 2 was skipped because the è takes two bytes.
+
+	var myStringInRuneArr = []rune(myString)
+	fmt.Printf("Length of a String after converting to Rune Arr is Valid = %v\n", len(myStringInRuneArr))
+
+	for i, v := range myStringInRuneArr {
+		fmt.Printf("%v => %v\n", i, v)
+	}
+
+	//String Concatenation
+
+	var strSlice = []string{"H", "E", "L", "L", "O"}
+
+	var catSlice = ""
+	for i := range strSlice {
+		catSlice += strSlice[i]
+	}
+
+	fmt.Printf("Concatenated String is = %v\n", catSlice)
+	//This is inefficient because a new arr is made on each concatenation
+
+	//Better String Concatenation Method
+	var strBuilder strings.Builder
+
+	for i := range strSlice {
+		strBuilder.WriteString(strSlice[i])
+	}
+
+	var catSlice2 = strBuilder.String()
+
+	fmt.Printf("Correctly built string = %v\n", catSlice2)
 
 }
